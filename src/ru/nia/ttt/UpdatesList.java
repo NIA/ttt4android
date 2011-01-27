0730fc631abc17fbcc7e85d18a4899d91bddded9
NIA <nia.fti@mail.ru>
2011-01-27 19:52:54 +0300

    Первый коммит
    Базовое Android-приложение, с наброском главной Activity.

---
+++ src/ru/nia/ttt/UpdatesList.java
@@ -0,0 +1,11 @@
+package ru.nia.ttt;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UpdatesList extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.updates_list);
+    }
+}
